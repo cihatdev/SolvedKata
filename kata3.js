9b3706c1a7d0bd9449e6de69f126c60bae431a86
cihatdev <cihatsalik1@gmail.com>
2020-09-08 18:55:09 +0300

    completed kata4, kata 5

--- kata3.js
+++ kata3.js
@@ -1,10 +1,29 @@
 // Basit, bir kelime dizisi verildiğinde, en kısa kelimelerin uzunluğunu döndürür.
 // Dize hiçbir zaman boş olmayacak ve farklı veri türlerini hesaba katmanız gerekmeyecek.
 
+// function findShort(s) {
+//   let words = s.split(" ");
+//   let shortest = words.reduce((shortestWord, currentWord) => {
+//     return currentWord.length < shortestWord.length ? currentWord : shortestWord;
+//   });
+//   let number = shortest.length;
+//   return number;
+// }
+
+// Other solutions1
+
+// function findShort(s){
+//   return Math.min.apply(null, s.split(' ').map(w => w.length));
+// }
+
+
+// Other solutions2
+
 function findShort(s) {
-  let words = s.spli
+  return s.split(' ').reduce((min, word) => Math.min(min, word.length), Infinity);
 }
 
-let output = findShort("Ahmet");
+
+let output = findShort("a ab abc abcd abcde abcdef");
 
 console.log(output);
