52aa02e8134bc599d01b715525af8d188943e761
cihatdev <cihatsalik1@gmail.com>
2020-09-08 14:17:05 +0300

    initial commit

---
+++ kata3.js
@@ -0,0 +1,10 @@
+// Basit, bir kelime dizisi verildiğinde, en kısa kelimelerin uzunluğunu döndürür.
+// Dize hiçbir zaman boş olmayacak ve farklı veri türlerini hesaba katmanız gerekmeyecek.
+
+function findShort(s) {
+  let words = s.spli
+}
+
+let output = findShort("Ahmet");
+
+console.log(output);
