bee6f5b38c517acb6e4fc31220e3d0117406360e
cihatdev <cihatsalik1@gmail.com>
2020-09-12 14:48:55 +0300

    solved  kata25 >> kata29

--- kata25.js
+++ kata25.js
@@ -10,37 +10,50 @@
 //   }
 // }
 
-function tickets(peopleInLine) {
-  var moneyPool = { 25: 0, 50: 0, 100: 0 };
-  var flag = true;
-  peopleInLine.every(function (value) {
-    moneyPool[value] += 1;
-    if (value === 50) {
-      if (moneyPool[25] > 0) {
-        moneyPool[25] -= 1;
-        return true;
-      }
-      flag = false;
-      return false;
-    } else if (value === 100) {
-      if (moneyPool[25] > 0 && moneyPool[50] > 0) {
-        moneyPool[25] -= 1;
-        moneyPool[50] -= 1;
-        return true;
-      }
-      if (moneyPool[25] > 2) {
-        moneyPool[25] -= 3;
-        return true;
-      }
+// function tickets(peopleInLine) {
+//   var moneyPool = { 25: 0, 50: 0, 100: 0 };
+//   var flag = true;
+//   peopleInLine.every(function (value) {
+//     moneyPool[value] += 1;
+//     if (value === 50) {
+//       if (moneyPool[25] > 0) {
+//         moneyPool[25] -= 1;
+//         return true;
+//       }
+//       flag = false;
+//       return false;
+//     } else if (value === 100) {
+//       if (moneyPool[25] > 0 && moneyPool[50] > 0) {
+//         moneyPool[25] -= 1;
+//         moneyPool[50] -= 1;
+//         return true;
+//       }
+//       if (moneyPool[25] > 2) {
+//         moneyPool[25] -= 3;
+//         return true;
+//       }
 
-      flag = false;
-      return false;
+//       flag = false;
+//       return false;
 
-    }
-    return true;
-  });
-  if (flag) return "YES";
-  else return "NO";
+//     }
+//     return true;
+//   });
+//   if (flag) return "YES";
+//   else return "NO";
+// }
+
+
+
+function tickets(peopleInLine){
+  let [c25,c50,c100] = [0,0,0];
+  for(let v of peopleInLine) {
+    if(v===25) c25++;
+    if(v===50) {c50++; c25--;}
+    if(v===100) {c25--; c50>0?c50--:c25-=2;}
+    if(c25<0||c50<0) return 'NO'
+  }
+  return 'YES'
 }
 
 
