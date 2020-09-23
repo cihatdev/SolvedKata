08b8cc2c3093676e054a850d8a87a8cfa8faa252
cihatdev <cihat02020202@gmail.com>
2020-09-23 17:18:02 +0300

    created typescript file

---
+++ TypeScript/ConvertBooleanValuesToStringsYesOr No.ts
@@ -0,0 +1,11 @@
+const boolToWord = (bool: boolean): string => {
+  let result: String = "";
+  if (bool === false) {
+    return "No";
+  } else {
+    ("Yes");
+  }
+};
+
+let out: String = boolToWord(false);
+console.log(out);
