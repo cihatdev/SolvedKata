6ba34fcda32f6f7adf68f97d65a76c45250c2d76
cihat <cihat02020202@gmail.com>
2021-05-03 15:51:27 +0300

    update katas folder name

--- data/SolvedKata/TypeScript/KeepHydrated.ts
+++
@@ -1,6 +0,0 @@
-export function litres(time: number): number {
-  return Math.floor(time / 2);
-}
-
-let out: number = litres(11.4);
-console.log(out);
--- data/js-solutions/096-validPhoneNumber.js
+++
@@ -1,6 +0,0 @@
-function validPhoneNumber(phoneNumber) {
-  return /^\(\d{3}\) \d{3}\-\d{4}$/.test(phoneNumber);
-}
-
-
-console.log('validPhoneNumber("(123) 456-7890")', validPhoneNumber("(123) 456-7890"))
